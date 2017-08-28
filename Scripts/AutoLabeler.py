022fb8db8e205c721e1a30d2f9dfdd9884179803
MinsuPark <alstn9173@gmail.com>
2017-08-28 16:10:22 +0900

    Auto Labeler v 1.0.1 -- on progress

--- Scripts/AutoLabeler.py
+++ Scripts/AutoLabeler.py
@@ -26,6 +26,8 @@ class AutoLabeler:
         self.input_file_directory = os.getcwd() + '/data/'
         self.output_file_directory = os.getcwd() + '/result/'
 
+        self.xml_source = {}
+
         self.desired_caps = desired_capabilities.get_desired_capabilities('')
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.desired_caps)
 
@@ -34,49 +36,12 @@ class AutoLabeler:
     # On the Construction #
     #######################
     def make_label_from_screen(self, file_name, num_of_image):
-        image_path = self.screenshot_from_appium(file_name + num_of_image)
+        file_path = self.output_file_directory + file_name + '_' + num_of_image
+        self.driver.save_screenshot(file_path + '.jpeg')
+        self.xml_source[file_name+num_of_image] = self.driver.page_source
+        self.do_action_use_appium(widget)
 
         #TODO implement state transition
-
-
-
-    ###############
-    # COMPLETE!!! #
-    ###############
-    def screenshot_from_appium(self, file_name):
-        self.driver.save_screenshot(self.output_file_directory + file_name)
-        return self.output_file_directory + file_name
-
-
-    #######################
-    # On the Construction #
-    #######################
-    def labeling_from_tensorflow(self, image_path):
-        # run tensorflow test method
-        result = 0
-        return result   # return refined label data
-
-
-    #######################
-    # On the Construction #
-    #######################
-    # result: output from the tensorflow
-    def refine_result_data(self, result):
-        label_data = []
-        for i in range(0, len(result)):
-            widget_type = result.get_widget_type()
-            x = result.get_boundary().get_coordinate()
-            y = result.get_boundary().get_coordinate()
-            width = result.get_boundary().get_width()
-            height = result.get_boundary().get_height()
-
-            line = widget_type + " 0.0 0 0.0 0 " \
-                   + x + " " + y + " " \
-                   + width + " " + height + " 0.0 0.0 0.0 0.0 0.0 0.0"
-
-            label_data.append(line)
-
-        return label_data
 
 
     ###############
@@ -90,30 +55,73 @@ class AutoLabeler:
             action = TouchAction(self.driver)
             action.tap(el).perform()
 
-    ###############
-    # COMPLETE!!! #
-    ###############
-    def save_label(self, filename, label):
-        label_data = open(self.output_file_directory + filename, 'w')
-        label_data.write(label)
-        label_data.close()
+
+    #######################
+    # On the Construction #
+    #######################
+    def labeling_from_tensorflow(self, image_path):
+        # run tensorflow test method
+        # TODO Fix Google Object Detection eval Method
+        # get box boundary data, class and accuracy
+
+        box_boundary = []
+        classes = []
+        accuracy = []
+
+        output = [box_boundary, classes, accuracy]
+
+        return output   # return refined label data
 
 
     #######################
     # On the Construction #
     #######################
-    def check_result(self, result):
-        xml = self.driver.page_source
-        widget_list = self.get_boundary_from_xml()
+    # result: output from the tensorflow
+    def refine_result_data(self, output):
+        label_data = []
+        for i in range(0, len(output)):
+            widget_type = output.get_widget_type()
+            x = output.get_boundary().get_coordinate()
+            y = output.get_boundary().get_coordinate()
+            width = output.get_boundary().get_width()
+            height = output.get_boundary().get_height()
+
+            line = widget_type + " 0.0 0 0.0 0 " \
+                   + x + " " + y + " " \
+                   + width + " " + height + " 0.0 0.0 0.0 0.0 0.0 0.0\n"
+
+            label_data.append(line)
+
+        return label_data
+
 
 
     ###############
     # COMPLETE!!! #
     ###############
-    def get_boundary_from_xml(self):
-        xml = self.driver.page_source
+    def save_label(self, filename, label):
+        label_file = open(self.output_file_directory + filename, 'w')
+        label_file.write(label)
+        label_file.close()
+        print('file [' + filename + '] generation complete')
 
-        parsing_data = Parser.Parser(xml)
+
+    #######################
+    # On the Construction #
+    #######################
+    def check_result(self, output, file_name):
+        parser = Parser.Parser(self.xml_source[file_name])
+        widget_list = parser.do_parsing()
+
+        for i in range(0, len(output)):
+            self.find_boundary_data(output[i], widget_list)
+
+
+    ###############
+    # COMPLETE!!! #
+    ###############
+    def get_boundary_from_xml(self, xml_source):
+        parsing_data = Parser.Parser(xml_source)
         widget_list = parsing_data.do_parsing()
 
         boundaries = []
@@ -135,8 +143,20 @@ class AutoLabeler:
             single_widget = widget_list[i]
             boundary = single_widget.get_widget_data('boundary')
 
-            for i in range(0, 4):
-                if boundary[i] - label_boundary[i] > 50 :
+            for j in range(0, len(boundary)):
+                if boundary[j] - label_boundary[j] > 50 :
                     return False
 
         return True
+
+
+if __name__ == '__main__':
+    auto_labeler = AutoLabeler()
+    application_name = ''
+    auto_labeler.make_label_from_screen(application_name, 0)
+
+    result = auto_labeler.labeling_from_tensorflow(os.getcwd() + '/result/')
+    label_data = auto_labeler.refine_result_data(result)
+    auto_labeler.save_label(application_name, label_data)
+
+    auto_labeler.check_result(label_data)
--- Scripts/widget.py
+++ Scripts/widget.py
@@ -4,27 +4,12 @@
 class Widget :
     def __init__(self, xml):
         self.xml = xml
-        self.widget_data = []
-        self.tag_name = ['index',       'text',         'class',
-                         'package',     'content-desc', 'checkable',
-                         'checked',     'enabled',      'focusable',
-                         'focused',     'scrollable',   'long-clickable',
-                         'password',    'selected',     'bounds',
-                         'resource-id', 'instance']
-
-        for i in range(0, len(self.tag_name)):
-            self.widget_data.append('')
+        self.widget_data = {}
 
     # Set tag information
     def set_widget_data(self, tag, value):
-        index = self.widget_data.index(tag)
-        self.widget_data[index] = value
+        self.widget_data[tag] = value
 
     # Returns tag information
     def get_widget_data(self, tag):
-        index = self.widget_data.index(tag)
-        return self.widget_data[index]
-
-    # A method that returns what tag an index comes in as input.
-    def what_tag_assigned_this_index(self, index):
-        return self.tag_name[index]
+        return self.widget_data[tag]
